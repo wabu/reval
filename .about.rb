d5d74da5fc59c470632ace67c02bd6ee2407c5e9
Daniel Waeber <wabu@fooserv.net>
2007-07-13 19:32:25 +0200

    to things put it on t3h web

---
+++ .about.rb
@@ -0,0 +1,26 @@
+require 'down'
+
+lambda do |r|
+    path = File.dirname(@filename)
+    tip = `cd '#{path}'; hg head`.split[1].split(':')[1]
+    unless r.omega.empty?
+        r.type, *snap = File.join(r.omega).split('.').reverse
+        snap = snap.reverse.join('.')
+        throw :download, `cd '#{path}'; hg archive -p #{snap} -r '#{snap.sub(/reval-/,'')}' -t '#{r.type}' -`
+    end
+    r.join(
+        r.small(" Check it out, download the ",
+             r.a('/pro/reval/'+tip.sub(/^/,'reval-')+'.tgz', 'latest tip'),
+            " or use #{r.a('/pro/hg/reval', 'hg-mercurial')} to get it."),
+        r.h4("reval - a relational algebra evaluater"),
+        r.p("reval is a leazy relational algebra interpreter written in haskell. You can get more
+             info inside the source or the generated doc files:"),
+        r.ul(
+            r.li( r.a("/pro/hg/x2c/file/", "browse") + " the tip"),
+            *Dir[File.join(path,'docs','*.html')].map do |f|
+                f.sub!(/#{path}/,'')
+                r.li(r.a("/pro/hg/reval/raw-file/#{tip}/#{f}",f))
+            end
+        )
+    )
+end
